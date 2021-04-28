2d6a9631f972989d2f582499c5702bb03177244d
Bonard Kibala <kibalabonard1@gmail.com>
2021-04-28 17:03:00 +0100

    mes amis robots

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import { Icon, Header, Image, Container, Grid, Segment, Form, Card, Input } from 'semantic-ui-react';
+import { Icon, Header, Image, Container, Grid, Segment, Form, Card, Input, Modal, Button } from 'semantic-ui-react';
 import { useEffect, useState } from 'react';
 import Cardrobot from './card';
+import React from 'react'
 
 const App = () => {
   const [friends, setFriends] = useState([])
   const [search, setSearch] = useState(friends)
   const [loading, setLoading] = useState(false)
+  const [open, setOpen] = React.useState(false)
+  const [modalFriend, setmodalFriend] = useState([])
 
   useEffect(() => {
     setLoading(true)
@@ -19,6 +22,12 @@ const App = () => {
       })
 
   }, [])
+
+  const currentinfo = (element) => {
+    setmodalFriend([element])
+  }
+  console.log(modalFriend)
+
   const handleChange = (e) => {
     e.preventDefault()
     let nameSearch = e.target.value.toUpperCase()
@@ -49,23 +58,52 @@ const App = () => {
                 </Form>
                 {/* {JSON.stringify(friends)} */}
                 {/* {JSON.stringify(search)} */}
+                {/* {JSON.stringify(modalFriend)} */}
                 <div className="cardContainer">
                   {
                     loading ? '' : search.map((friend, index) => <div>
-                      <Card key={index} className="card">
-                        <Image src={`https://robohash.org/${friend.id}`} wrapped ui={true} />
-                        <Card.Content>
-                          <Card.Header>{friend.name}</Card.Header>
-                          <Card.Description>
-                            {friend.email}
-                          </Card.Description>
-                        </Card.Content>
-                      </Card> <br></br>
-                      {/* <Cardrobot key={index} src={`https://robohash.org/${friend.id}`} name={friend.name} email={friend.email} /> */}
-                    </div>)
+
+                      {/* ------------------Modal--------------------- */}
+
+                      <Modal
+                        onClose={() => setOpen(false)}
+                        onOpen={() => setOpen(true)}
+                        open={open}
+                        trigger={<div><Card key={index} className="card" onClick={() => currentinfo({id:friend.id, name: friend.name, email: friend.email,street:friend.address.street
+                         ,suite:friend.address.suite,city:friend.address.city,zipcode:friend.address.zipcode,phone:friend.phone,website:friend.website
+                         ,companyName:friend.company.name})}>
+                          <Image src={`https://robohash.org/${friend.id}`} wrapped ui={true} />
+                          <Card.Content>
+                            <Card.Header>{friend.name}</Card.Header>
+                            <Card.Description>
+                              {friend.email}
+                            </Card.Description>
+                          </Card.Content>
+                        </Card> <br></br></div>}
+                      >
+                           {modalFriend.map(elt=><> <Modal.Header>{elt.name}</Modal.Header>
+                          <Modal.Content image>
+                            <Image size='medium' src={`https://robohash.org/${elt.id}`} wrapped />
+                            <Modal.Description>
+                              <address>
+                             E-mail: {elt.email} <br></br>
+                            <span>Adress:</span> {`${elt.street} street, ${elt.suite} suite, ${elt.city} city, ${elt.zipcode} zipcode`} <br></br>
+                              Phone: {elt.phone} <br></br>
+                              Website: {elt.website}<br></br>
+                              CompanyName: {elt.companyName}
+                              </address>
+                              <p></p>
+                            </Modal.Description>
+                          </Modal.Content>
+                          </>)}
+                        <Modal.Actions>
+                          <Button color='red' onClick={() => setOpen(false)}>Cancel</Button>
+                        </Modal.Actions>
+                      </Modal>
+                      {/* -------------------End modal----------------- */} </div>)
                   }
                 </div>
-                
+
               </Segment>
             </Grid.Column>
           </Grid>
